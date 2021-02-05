33eff8a0268a9febfe12ad42f8b3ec3eb784b1d2
Ivan <ivan.prieto.kommit@gmail.com>
2021-02-05 08:51:58 -0500

    Add functionality for users to have followers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   root "articles#index"
+=======
+  root 'sessions#welcome'
+  resources :users
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
+  delete 'logout', to: 'sessions#destroy'
+  get 'users/:id/follow', to: 'users#follow', as: 'follow_user'
+  get 'users/:id/unfollow', to: 'users#unfollow', as: 'unfollow_user'
+  post 'users/:id/follow', to: 'users#follow'
+  post 'users/:id/unfollow', to: 'users#unfollow'
+>>>>>>> Stashed changes
 
   resources :articles do
     resources :comments
