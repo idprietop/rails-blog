49db0e2e4dcf23a8a56c0d9da61cf256d2e55b3e
Ivan <ivan.prieto.kommit@gmail.com>
2021-02-10 07:58:15 -0500

    Rename creator_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :logged_in?
-  helper_method :creator_user
+  helper_method :find_article_owner
 
   def current_user
     User.find_by(id: session[:user_id])
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
     !current_user.nil?
   end
 
-  def creator_user(user_id)
+  def find_article_owner(user_id)
     User.find_by(id: user_id)
   end
 end
